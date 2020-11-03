4388e1f080128dee4a99ad458c5478ea0d1747e2
Sunethra <37868833+Sunethra-afk@users.noreply.github.com>
2020-11-03 11:45:16 +0530

    Create 3_nov_lab9.R

---
+++ 3_nov_lab9.R
@@ -0,0 +1,4 @@
+#3rd nov 2020 - lab
+library(ggplot2)
+mtcars
+dotchart(mtcars$mpg, labels=row.names(mtcars), main="Cars VS MPG Values", xlab="mpg", ylab="cars", xlim=c(0,40), pch=25, cex=1, col="red", lcol="green")
